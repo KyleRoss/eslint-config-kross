ecaae0561345445f6b5a49da6587bfa4f2050580
Kyle Ross <kylerross1324@gmail.com>
2022-05-19 15:42:57 -0400

    feat: move base rule overrides out so they can be reusable

---
+++ baseRules.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'array-element-newline': 'off',
+  'arrow-body-style': 'off',
+  camelcase: ['error', { properties: 'never' }],
+  'capitalized-comments': 'off',
+  'comma-dangle': ['error', 'never'],
+  curly: ['error', 'multi-line'],
+  'func-names': ['error', 'as-needed'],
+  'object-curly-spacing': ['error', 'always'],
+  'operator-linebreak': ['error', 'after'],
+  'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
+  'no-negated-condition': 'off',
+  'no-implicit-coercion': 'off',
+  'keyword-spacing': ['error', {
+    overrides: {
+      if: { after: false },
+      for: { after: false },
+      while: { after: false },
+      catch: { after: false }
+    }
+  }]
+};
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const rules = require('./baseRules');
+
 module.exports = {
   ignorePatterns: [
     'node_modules/**/*',
@@ -12,26 +14,5 @@ module.exports = {
     'xo-space'
   ],
 
-  rules: {
-    'array-element-newline': 'off',
-    'arrow-body-style': 'off',
-    camelcase: ['error', { properties: 'never' }],
-    'capitalized-comments': 'off',
-    'comma-dangle': ['error', 'never'],
-    curly: ['error', 'multi-line'],
-    'func-names': ['error', 'as-needed'],
-    'object-curly-spacing': ['error', 'always'],
-    'operator-linebreak': ['error', 'after'],
-    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
-    'no-negated-condition': 'off',
-    'no-implicit-coercion': 'off',
-    'keyword-spacing': ['error', {
-      overrides: {
-        if: { after: false },
-        for: { after: false },
-        while: { after: false },
-        catch: { after: false }
-      }
-    }]
-  }
+  rules
 };
